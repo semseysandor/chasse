cf90a56f1087e88c80ecf0f22a2799a3d65dbbb1
Sandor Semsey <semseysandor@gmail.com>
2020-07-29 17:56:44 +0200

    summary

---
+++ CRM/Chasse/Page/Summary.php
@@ -0,0 +1,48 @@
+<?php
+use CRM_Chasse_ExtensionUtil as E;
+
+class CRM_Chasse_Page_Summary extends CRM_Core_Page {
+
+  public function run() {
+
+    if (!CRM_Core_Permission::check('access CiviMail')) {
+      CRM_Core_Error::statusBounce(ts('You do not have permission to view this page.'));
+    }
+    CRM_Utils_System::setTitle(E::ts('Journey Summary'));
+
+    $journeys_all=Civi::settings()->get('chasse_config');
+
+    if (is_null($journeys_all)) {
+      $this->assign('no_journeys',true);
+      parent::run();
+      return;
+    }
+
+
+    foreach ($journeys_all['journeys'] as $journey)
+    {
+      $name=$journey['name'];
+      $id=$journey['id'];
+      $steps=$journey['steps'];
+      foreach ($steps as $step) {
+        $journeys[]=[
+          'id' => $id,
+          'name' => $name,
+          'code' => $step['code'],
+          'msg_template_id' => $step['send_mailing'],
+        ];
+      }
+    }
+
+
+
+    $this->assign('journeys',$journeys);
+    parent::run();
+  }
+
+  protected function getJourneys()
+  {
+    $journeys=[];
+  }
+
+}
---
+++ templates/CRM/Chasse/Page/Summary.tpl
@@ -0,0 +1,31 @@
+<h3>Journey Summary</h3>
+
+{if $no_journeys}
+  no journeys madafaka!
+
+{else}
+
+<table class="selector row-highlight">
+  <thead class="sticky">
+    <th>id</th>
+    <th>Journey</th>
+    <th>Steps</th>
+    <th>mailing_id</th>
+    <th></th>
+  </thead>
+
+  {foreach from=$journeys item="journey"}
+    <tr class="{cycle values="odd-row,even-row"} crm-mailing">
+      <td class="crm-mailing-status">{$journey.id}</td>
+      <td class="crm-mailing-status">{$journey.name}</td>
+      <td class="crm-mailing-status">{$journey.code}</td>
+      <td class="crm-mailing-status">{$journey.mailing_id}</td>
+      <td>
+        <a href ={crmURL p='civicrm/chasse/report' q="msg_template_id="}{$journey.msg_template_id} title="Show report">
+        Report
+        </a>
+      </td>
+    </tr>
+  {/foreach}
+</table>
+{/if}
